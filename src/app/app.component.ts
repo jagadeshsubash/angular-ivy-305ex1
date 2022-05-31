7af1ce19dccac25f7d60abfcac0f9d3658ab33b0
jagadeshsubash <subash.jagadesh@gmail.com>
2022-05-31 11:56:12 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component, VERSION } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+  name = 'Angular ' + VERSION.major  + " @ " + VERSION.minor;
+  title =  "Geek or Freak";
+
+
 }
